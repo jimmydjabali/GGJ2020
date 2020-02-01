8c0819ca40f4686fa3b28b89da69618ad63a4afe
Jimmy <jimmy.djabali@epitech.eu>
2020-02-01 18:57:52 +0100

    player sprite

--- Assets/Script/Rotation.cs
+++ Assets/Script/Rotation.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotation : MonoBehaviour
+public class Rotation : MonoBehaviour
 {
     Vector3 currentEulerAngles;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,5 +22,9 @@ public class rotation : MonoBehaviour
         float angle = Mathf.Atan2(xAxis, yAxis) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 180.0f));
     }
-    
+
+    public void setMoving(bool isMoving)
+    {
+
+    }
 }
