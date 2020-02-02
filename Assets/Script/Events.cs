f181e700a8d53deae882e118353e3345f5e92225
Jimmy <jimmy.djabali@epitech.eu>
2020-02-02 14:55:29 +0100

    qte is ok

--- Assets/Script/Events.cs
+++ Assets/Script/Events.cs
@@ -4,50 +4,68 @@ using UnityEngine;
 
 public class Events : MonoBehaviour
 {
-    public float minEventTime = 1.0f;
-    public float maxEventTime = 3.0f;
+    public float minEventTime;
+    public float maxEventTime;
 
     public Transform fireObject;
 
-    private Vector3[] eventsPositions = {
-        new Vector3(2.26f, -1.05f, 1.0f),
-        new Vector3(-2.19f, -1.09f, 1.0f),
-        new Vector3(-2.19f, -2.09f, 1.0f),
-        new Vector3(-3.19f, -1.09f, 1.0f)
-    };
+    public int fireState = 0;
 
-    private List<int> currentEvents = new List<int>();
+    private List<Vector3> fireEventPositions = new List<Vector3>();
+    private List<Vector3> dropEventPositions = new List<Vector3>();
+
+    private List<int> currentFireEvents = new List<int>();
+    private List<int> currentDropEvents = new List<int>();
+    private List<int> currentElectricEvents = new List<int>();
+    private List<int> currentAirEvents = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
+        fireEventPositions.Add(new Vector3(-3.44f, 1.59f, 1.0f));
+        fireEventPositions.Add(new Vector3(-2.94f, -0.32f, 1.0f));
+        fireEventPositions.Add(new Vector3(2.65f, -2.07f, 1.0f));
+        fireEventPositions.Add(new Vector3(3.46f, 1.07f, 1.0f));
+
+        dropEventPositions.Add(new Vector3(-0.92f, -3.8f, 1.0f));
+        dropEventPositions.Add(new Vector3(0.93f, -3.8f, 1.0f));
+        dropEventPositions.Add(new Vector3(0.0f, -4.73f, 1.0f));
+
         callEvent("FireEvent");
+    }
+    
+    public void DeleteEvent(Vector3 position)
+    {
+        currentFireEvents.Remove(fireEventPositions.IndexOf(position));
+        fireState -= 1;
     }
     
     void callEvent(string functionName)
     {
-        Invoke(functionName, Random.Range(minEventTime, maxEventTime));
+        float time = Random.Range(minEventTime, maxEventTime);
+        Invoke(functionName, time);
     }
 
-    int pickRandomPosition()
+    int pickRandomPosition(List<Vector3> positions, List<int> currentEvents)
     {
         int randomPosition = 0;
-        do
-        {
-            randomPosition = Random.Range(0, eventsPositions.Length);
-        } while (currentEvents.Contains(randomPosition));
+        do {
+            randomPosition = Random.Range(0, positions.Count);
+        } while (currentFireEvents.Contains(randomPosition));
         currentEvents.Add(randomPosition);
         return randomPosition;
     }
 
     void FireEvent()
     {
-        int randomPosition = pickRandomPosition();
-        Debug.Log(randomPosition);
+        callEvent("FireEvent");
+        Debug.Log(fireEventPositions.Count);
+        if (currentFireEvents.Count >= fireEventPositions.Count) return;
+        int randomPosition = pickRandomPosition(fireEventPositions, currentFireEvents);
         Transform fire = Instantiate(fireObject, transform);
-        fire.localPosition = eventsPositions[randomPosition];
+        fire.localPosition = fireEventPositions[randomPosition];
         fire.gameObject.SetActive(true);
-        //callEvent("FireEvent");
+        fireState = currentFireEvents.Count;
     }
 
     // Update is called once per frame
---
+++ Assets/Script/QTE.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTE : MonoBehaviour
+{
+    public Sprite A;
+    public Sprite B;
+    public Sprite X;
+    public Sprite Y;
+    public int qteSize = 5;
+    public float time = 1.5f;
+    public Player2 player2Obj;
+
+    private List<int> buttonCombination = new List<int>();
+    private int currentPosition = 0;
+    private bool isQteStarted = false;
+    private float remainingTime;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isQteStarted) return;
+
+        remainingTime -= Time.deltaTime;
+        if (remainingTime < 0)
+        {
+            gameObject.SetActive(false);
+            currentPosition = 0;
+            player2Obj.failedQte();
+        }
+
+        int newCurrentPostion = currentPosition;
+
+        if (Input.GetButtonDown("A Button"))
+        {
+            if (buttonCombination[currentPosition] == 0)
+                newCurrentPostion += 1;
+            else
+            {
+                gameObject.SetActive(false);
+                currentPosition = 0;
+                player2Obj.failedQte();
+            }
+        }
+        if (Input.GetButtonDown("B Button"))
+        {
+            if (buttonCombination[currentPosition] == 1)
+                newCurrentPostion += 1;
+            else
+            {
+                gameObject.SetActive(false);
+                currentPosition = 0;
+                player2Obj.failedQte();
+            }
+        }
+        if (Input.GetButtonDown("X Button"))
+        {
+            if (buttonCombination[currentPosition] == 2)
+                newCurrentPostion += 1;
+            else
+            {
+                gameObject.SetActive(false);
+                currentPosition = 0;
+                player2Obj.failedQte();
+            }
+        }
+        if (Input.GetButtonDown("Y Button"))
+        {
+            if (buttonCombination[currentPosition] == 3)
+                newCurrentPostion += 1;
+            else
+            {
+                gameObject.SetActive(false);
+                currentPosition = 0;
+                player2Obj.failedQte();
+            }
+        }
+
+        if (newCurrentPostion != currentPosition)
+        {
+            currentPosition = newCurrentPostion;
+            remainingTime = time;
+            if (buttonCombination[currentPosition] == 0) GetComponent<SpriteRenderer>().sprite = A;
+            if (buttonCombination[currentPosition] == 1) GetComponent<SpriteRenderer>().sprite = B;
+            if (buttonCombination[currentPosition] == 2) GetComponent<SpriteRenderer>().sprite = X;
+            if (buttonCombination[currentPosition] == 3) GetComponent<SpriteRenderer>().sprite = Y;
+        }
+        
+        if (currentPosition > qteSize - 1)
+        {
+            gameObject.SetActive(false);
+            currentPosition = 0;
+            player2Obj.successQte();
+        }
+    }
+
+    public void startQte()
+    {
+        int button;
+        while (buttonCombination.Count < qteSize)
+        {
+            do
+            {
+                button = Random.Range(0, 4);
+            } while (buttonCombination.Count > 0 && button == buttonCombination[buttonCombination.Count - 1]);
+            buttonCombination.Add(button);
+        }
+        if (buttonCombination[currentPosition] == 0) GetComponent<SpriteRenderer>().sprite = A;
+        if (buttonCombination[currentPosition] == 1) GetComponent<SpriteRenderer>().sprite = B;
+        if (buttonCombination[currentPosition] == 2) GetComponent<SpriteRenderer>().sprite = X;
+        if (buttonCombination[currentPosition] == 3) GetComponent<SpriteRenderer>().sprite = Y;
+        gameObject.SetActive(true);
+        isQteStarted = true;
+        remainingTime = time;
+    }
+}
